381d782f52c6af8bb21be65c93c66b16bf473d3e
Dale-Heron <heron_dale@yahoo.co.uk>
2024-11-19 14:52:21 +0000

    get server hosting correct to trigger wasm project

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-//builder.Services.AddRazorPages();
+builder.Services.AddRazorPages();
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,12 +17,23 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseWebAssemblyDebugging();
 }
 
 app.UseHttpsRedirection();
 
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+//app.UseRouting();
+
 app.UseAuthorization();
 
+app.MapRazorPages();
+
 app.MapControllers();
+
+app.MapFallbackToFile("index.html");
+
 
 app.Run();
