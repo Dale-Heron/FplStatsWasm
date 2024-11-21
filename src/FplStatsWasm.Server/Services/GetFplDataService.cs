f26cc9eac86ca2faa40d80c2fd3797807bfb02f1
Dale-Heron <heron_dale@yahoo.co.uk>
2024-11-21 15:34:12 +0000

    tidy

--- src/FplStatsWasm.Server/Services/GetFplDataService.cs
+++ src/FplStatsWasm.Server/Services/GetFplDataService.cs
@@ -23,15 +23,17 @@ public class GetFplDataService
 
     public async Task<List<Player>> GetPlayersData()
     {
-        List<Player> players = new List<Player>();
+        var players = new List<Player>();
         string url = "https://fantasy.premierleague.com/api/bootstrap-static/";
         
         var topLevel = await httpClient.GetFromJsonAsync<Dictionary<string, JsonElement>>(url);
 
-        if( topLevel != null){
+        if( topLevel != null)
+        {
             var tempPlayers = JsonSerializer.Deserialize<List<Player>>(topLevel["elements"]);
 
-            if(tempPlayers != null){
+            if(tempPlayers != null)
+            {
                 _logger.LogInformation("Count={Count}", tempPlayers.Count);
                 players = tempPlayers;
             }
