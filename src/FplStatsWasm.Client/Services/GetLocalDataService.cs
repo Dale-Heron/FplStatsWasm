baeb3b0c0fad7e001823faa2f3c715681c55ac1d
Dale-Heron <heron_dale@yahoo.co.uk>
2024-11-21 16:58:37 +0000

    tidy warnings for client

--- src/FplStatsWasm.Client/Services/GetLocalDataService.cs
+++ src/FplStatsWasm.Client/Services/GetLocalDataService.cs
@@ -1,23 +1,22 @@
 using System.Net.Http.Json;
 using FplStatsWasm.Shared.Models;
 
-
 namespace FplStatsWasm.Client.Services;
 
 public class GetLocalDataService
 {
-    private HttpClient httpClient;
+    private readonly HttpClient _httpClient;
     
     public GetLocalDataService(IHttpClientFactory httpClientFactory)
     {
-        httpClient = httpClientFactory.CreateClient("backend");
+        _httpClient = httpClientFactory.CreateClient("backend");
     }
 
     public async Task<List<Player>> GetPlayers()
     {
         string url = "/Players";
         
-        List<Player>? players = await httpClient.GetFromJsonAsync<List<Player>>(url);
+        List<Player>? players = await _httpClient.GetFromJsonAsync<List<Player>>(url);
 
         if(players!=null && players.Count>0){
             return players;
