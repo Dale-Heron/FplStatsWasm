71797a1aa24fc4f6daea660987885f67a434d4a1
Dale-Heron <heron_dale@yahoo.co.uk>
2024-11-22 16:06:52 +0000

    better server tests

--- test/FplStatsWasm.Server.Tests/GetFplDataServiceTests.cs
+++ test/FplStatsWasm.Server.Tests/GetFplDataServiceTests.cs
@@ -17,50 +17,53 @@ public class GetFplDataServiceTests
     }
 
     [Fact]
-    public async void GetPlayersData_happy_path()
+    public async void GetPlayersData_Use_real_httpclient_and_memory_cache_Check_log_message_and_data()
     {
         //arrange
-        var service = GetService(out var logger);
+        var (service, logger) = GetFplDataService();
     
         //act
         var players = await service.GetPlayersData();
 
-
         //assert
         Assert.NotEmpty(players);
-        Assert.NotNull(logger.Collector.LatestRecord);
-        Assert.Equal(1, logger.Collector.Count);
-        Assert.Equal(LogLevel.Information, logger.Collector.GetSnapshot()[0].Level);
-        Assert.StartsWith("Found ", logger.Collector.LatestRecord.Message);
+
+        CheckForSingleFoundLogMessage(logger);
         
         _output.WriteLine(logger.Collector.LatestRecord.Message);
     }
     
     [Fact]
-    public async void GetPlayersData_Call_twice_should_cache()
+    public async void GetPlayersData_Use_real_httpclient_and_memory_cache_Call_twice_should_cache()
     {
         //arrange
-        var service = GetService(out var logger);
+        var (service, logger) = GetFplDataService();
 
         //act
         await service.GetPlayersData();
-        
+
         var players = await service.GetPlayersData();
 
 
         //assert
         Assert.NotEmpty(players);
+
+        CheckForSingleFoundLogMessage(logger);
+
+        _output.WriteLine(logger.Collector.LatestRecord.Message);
+    }
+
+    private static void CheckForSingleFoundLogMessage(FakeLogger<GetFplDataService> logger)
+    {
         Assert.NotNull(logger.Collector.LatestRecord);
         Assert.Equal(1, logger.Collector.Count);
         Assert.Equal(LogLevel.Information, logger.Collector.GetSnapshot()[0].Level);
         Assert.StartsWith("Found ", logger.Collector.LatestRecord.Message);
-        
-        _output.WriteLine(logger.Collector.LatestRecord.Message);
     }
 
-    private static GetFplDataService GetService(out FakeLogger<GetFplDataService> logger)
+    private static (GetFplDataService service, FakeLogger<GetFplDataService> logger) GetFplDataService()
     {
-        logger = new FakeLogger<GetFplDataService>();
+        var logger = new FakeLogger<GetFplDataService>();
 
         var hcFactory = new Mock<IHttpClientFactory>();
         hcFactory.Setup(hcf => hcf.CreateClient(string.Empty))
@@ -69,6 +72,6 @@ public class GetFplDataServiceTests
         var inmemory = new MemoryCache(new MemoryCacheOptions());
 
         var service = new GetFplDataService(inmemory, logger, hcFactory.Object);
-        return service;
+        return (service, logger);
     }
 }
